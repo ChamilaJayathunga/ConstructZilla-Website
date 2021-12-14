dceec526814a72484da45167300dd50653b08375
Chamila Jayathunga <90988121+ChamilaJayathunga@users.noreply.github.com>
2021-12-15 00:22:40 +0530

    Add files via upload

---
+++ dropzone/db_conn.php
@@ -0,0 +1,15 @@
+<?php 
+    session_start();
+    $con=mysqli_connect("localhost","root","","construction2");
+    define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/ConstructZilla/');
+    define('SITE_PATH','http://127.0.0.1/ConstructZilla/');
+
+    define('MEMBER_IMAGE_SERVER_PATH',SERVER_PATH.'media/nic/');
+    define('MEMBER_IMAGE_SITE_PATH',SITE_PATH.'media/nic/');
+
+    define('USER_IMAGE_SERVER_PATH',SERVER_PATH.'media/user_image/');
+    define('USER_IMAGE_SITE_PATH',SITE_PATH.'media/user_image/');
+
+    define('PROOF_IMAGE_SERVER_PATH',SERVER_PATH.'media/proof/');
+    define('PROOF_IMAGE_SITE_PATH',SITE_PATH.'media/proof/');
+?>
